a2c72925f8c4613d34cf75997712e8bfd166ab71
Mahnoor Khan <mahnoorkhan@tintash.com>
2019-12-03 16:26:47 +0500

    Clocks aligned.

--- OKR-CoreAnimation/OKR-CoreAnimation/Animation1/Animation1.swift
+++ OKR-CoreAnimation/OKR-CoreAnimation/Animation1/Animation1.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class Animation1: UIViewController {
+    
+    var clockView           : ClockView?
+    var clockWithNumbers    : ClockWithNumbers?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,20 +38,22 @@ extension Animation1 {
     func addClock() {
         let width: CGFloat = 240.0
         let height: CGFloat = 240.0
-        let clockView = ClockView(frame: CGRect(x: self.view.frame.size.width/2 - width/2, y: 2.5 * (self.navigationController?.navigationBar.frame.height ?? 100), width: width, height: height))
-        clockView.backgroundColor = .clear
-        clockView.layer.cornerRadius = clockView.frame.width/2
-        clockView.layer.masksToBounds = true
+        clockView = ClockView(frame: CGRect(x: self.view.frame.size.width/2 - width/2, y: 2.5 * (self.navigationController?.navigationBar.frame.height ?? 100), width: width, height: height))
+        clockView?.backgroundColor = .clear
+        clockView?.layer.cornerRadius = (clockView?.frame.width ?? 0)/2
+        clockView?.layer.masksToBounds = true
+        guard let clockView  = self.clockView else { return }
         self.view.addSubview(clockView)
     }
     
     func addNumberClock() {
         let width: CGFloat = 240.0
         let height: CGFloat = 240.0
-        let clockView = ClockWithNumbers(frame: CGRect(x: self.view.frame.size.width/2 - width/2, y: self.view.frame.size.height - height - 20, width: width, height: height))
-        clockView.backgroundColor = .clear
-        clockView.layer.cornerRadius = clockView.frame.width/2
-        clockView.layer.masksToBounds = true
-        self.view.addSubview(clockView)
+        clockWithNumbers = ClockWithNumbers(frame: CGRect(x: self.view.frame.size.width/2 - width/2, y: (2.5 * (self.navigationController?.navigationBar.frame.height ?? 100)) + ((clockView?.frame.height ?? 0) + 40), width: width, height: height))
+        clockWithNumbers?.backgroundColor = .clear
+        clockWithNumbers?.layer.cornerRadius = (clockWithNumbers?.frame.width ?? 0)/2
+        clockWithNumbers?.layer.masksToBounds = true
+        guard let clockWithNumbers  = self.clockWithNumbers else { return }
+        self.view.addSubview(clockWithNumbers)
     }
 }
