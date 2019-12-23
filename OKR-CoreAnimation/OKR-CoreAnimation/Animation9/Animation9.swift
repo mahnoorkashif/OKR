2a47f1977fd8d73ba01fbba9511167b23f05d854
Mahnoor Khan <mahnoorkhan@tintash.com>
2019-12-23 17:17:53 +0500

    Changes in last animation.

--- OKR-CoreAnimation/OKR-CoreAnimation/Animation9/Animation9.swift
+++ OKR-CoreAnimation/OKR-CoreAnimation/Animation9/Animation9.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class Animation9: UIViewController {
     var timer : Timer?
+    var index : Int = 0
     var count : Int = 0
     
     var paths : [UIBezierPath] = []
@@ -18,13 +19,13 @@ class Animation9: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         createPaths()
-        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
 }
 
 extension Animation9 {
     private func createPaths() {
-        let diff = Int((self.view.frame.width) / 100)
+        let diff = Int((self.view.frame.width) / 20)
         
         var i = diff
         let width = Int(self.view.frame.width)
@@ -53,7 +54,7 @@ extension Animation9 {
     }
     
     private func createStar() -> UIImageView {
-        let imageView = UIImageView(frame: CGRect(x: points[count].x, y: points[count].y, width: 20, height: 20))
+        let imageView = UIImageView(frame: CGRect(x: points[index].x, y: points[index].y, width: 20, height: 20))
         imageView.image = #imageLiteral(resourceName: "Star")
         imageView.image = imageView.image?.withRenderingMode(.alwaysTemplate)
         imageView.tintColor = .random
@@ -64,6 +65,7 @@ extension Animation9 {
         let animation = CAKeyframeAnimation(keyPath: "position")
         animation.duration = duration
         animation.path = path.cgPath
+        animation.repeatCount = Float.infinity
         animation.calculationMode = .paced
         animation.fillMode = .forwards
         animation.isRemovedOnCompletion = false
@@ -77,9 +79,16 @@ extension Animation9 {
 }
     
     @objc private func updateTimer() {
-        count = Int.random(in: 0...(paths.count - 1))
-        let starView = createStar()
-        self.view.addSubview(starView)
-        createPositionAnimation(path: paths[count], starView: starView, duration: 5)
+        if count == 200 {
+            timer?.invalidate()
+            timer = nil
+            count = 0
+        } else {
+            index = Int.random(in: 0...(paths.count - 1))
+            let starView = createStar()
+            self.view.addSubview(starView)
+            createPositionAnimation(path: paths[index], starView: starView, duration: 5)
+            count += 1
+        }
     }
 }
